9cfd31dbdc9015d4cc8c550bf8255d48259ee423
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-17 19:27:30 +0200

    plugin to deploy document /static/docs

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-05-17T19:15:46+0200",
+    date = "2020-05-17T19:24:56+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.1 (Oracle Corporation)"
 )
 @Component
